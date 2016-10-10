03ac527833a1d96bce0df47d8f0705dde51c51b2
kdex <kdex@kdex.de>
2016-10-10 12:34:25 +0200

    Fixed bug where specialized events would get fired more than once

--- src/Client.js
+++ src/Client.js
@@ -1,12 +1,10 @@
 import EventEmitter from "crystal-event-emitter";
-import {
-	default as RPCClient,
-	ANY,
-	MESSAGE_ACKNOWLEDGEMENT,
-	MESSAGE_REPLY
-} from "ws-rpc-client";
+import RPCClient, { ANY } from "ws-rpc-client";
 const extensions = Symbol();
 export class Client extends EventEmitter {
+	ws = null;
+	reconnecting = false;
+	wasOpenBefore = false;
 	constructor(url, protocols, {
 		autoReconnect = true,
 		reconnectionFactor = 1.2,
@@ -29,56 +27,60 @@ export class Client extends EventEmitter {
 		this.emit("close", e);
 		this.ws = null;
 	}
-	open() {
-		return new Promise((resolve, reject) => {
-			let closed = false;
-			if (this.ws) {
-				closed = this.close();
-			}
+	async open() {
+		return new Promise(async (resolve, reject) => {
 			/* Wait for already connected client to close first */
-			Promise.resolve(closed).then(() => {
-				if (typeof WebSocket === "undefined") {
-					/* "require" shouldn't be transpiled here, so enjoy this hack for node support */
-					global.WebSocket = eval(`require("ws")`);
+			if (this.ws) {
+				await this.close();
+			}
+			if (!global.WebSocket) {
+				global.WebSocket = eval(`require("ws")`);
+			}
+			this.ws = new WebSocket(this[extensions].url, this[extensions].protocols);
+			this.rpcClient = new RPCClient(this.ws, this[extensions].rpcOptions);
+			this.ws.onopen = e => {
+				this.emit("open", e);
+				this.wasOpenBefore = true;
+				resolve(e);
+			};
+			this.ws.onerror = e => {
+				this.emit("error", e);
+				reject(e);
+			};
+			/* Closed dirtily */
+			this.ws.onclose = e => {
+				this.resetWebsocket(e);
+				if (!this.wasOpenBefore) {
+					this.emit("connectionFailed", e);
 				}
-				this.ws = new WebSocket(this[extensions].url, this[extensions].protocols);
-				this.rpcClient = new RPCClient(this.ws, this[extensions].rpcOptions);
-				this.ws.onopen = e => {
-					this.emit("open", e);
-					this.hasBeenOpenBefore = true;
-					resolve(e);
-				};
-				this.ws.onerror = e => {
-					this.emit("error", e);
-					reject(e);
-				};
-				this.rpcClient.on(ANY, e => {
-					if (!e.data) {
-						/* Proxy message event */
-						this.emit("message", e);
+				if (this[extensions].autoReconnect && !this.reconnecting) {
+					this.reconnect();
+				}
+			};
+			this.rpcClient.on(ANY, e => {
+				const { data } = e;
+				if (data) {
+					if (e.raw) {
+						/* Proxy generalized `message` event */
+						this.emit("message", data);
 					}
 					else {
-						/* Proxy specialized message event */
-						const instruction = e.data.payload.instruction;
-						if (instruction !== MESSAGE_REPLY && instruction !== MESSAGE_ACKNOWLEDGEMENT) {
-							this.emit(instruction, e.data);
-						}
+						/* Proxy specialized instruction event */
+						const { instruction } = data.payload;
+						this.emit(instruction, data);
 					}
-				});
-				/* Closed dirtily */
-				this.ws.onclose = e => {
-					this.resetWebsocket(e);
-					if (!this.hasBeenOpenBefore) {
-						this.emit("connectionFailed", e);
-					}
-					if (this[extensions].autoReconnect && !this.reconnecting) {
-						this.reconnect();
-					}
-				};
+				}
+				else {
+					/*
+					* Don't do anything. No data ⇒ It's not a message event.
+					* This case should be handled by the event listeners on
+					* `this.ws` already.
+					*/
+				}
 			});
 		});
 	}
-	close() {
+	async close() {
 		return new Promise((resolve, reject) => {
 			if (this.ws) {
 				/* Closed cleanly */
@@ -98,9 +100,9 @@ export class Client extends EventEmitter {
 			}
 		});
 	}
-	async reconnect(newWaitingTime) {
+	async reconnect(newDelay) {
 		this.reconnecting = true;
-		let waitingTime = newWaitingTime || this[extensions].reconnectionMinimum;
+		const delay = newDelay || this[extensions].reconnectionMinimum;
 		try {
 			await this.open();
 			this.reconnecting = false;
@@ -108,8 +110,8 @@ export class Client extends EventEmitter {
 		}
 		catch (e) {
 			setTimeout(() => {
-				this.reconnect(waitingTime * this[extensions].reconnectionFactor);
-			}, waitingTime);
+				this.reconnect(delay * this[extensions].reconnectionFactor);
+			}, delay);
 		}
 	}
 	async send(payload) {
